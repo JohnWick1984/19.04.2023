c0e5cad698c5967f520f1e7f8dbb9091d052acd4
strupeiq <alinka-roxy@mail.ru>
2023-04-19 21:33:26 +0500

    Добавьте файлы проекта.

---
+++ 19.04.2023/19.04.2023.cpp
@@ -0,0 +1,55 @@
+﻿// 19.04.2023.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Data {
+	int day;
+	string month;
+	int year;
+public:
+	Data(int day, string month, int year) {
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	};
+
+	void PrintData()
+	{
+		cout << day << month << year;
+	
+	}
+};
+
+class Human {
+public:
+
+	int ID;
+	string LastName;
+	string Name;
+	string MiddleName;
+    Data& data;
+	Human(int ID, string LastName, string Name, string MiddleName, Data& data) :ID(ID), LastName(LastName), Name(Name), MiddleName(MiddleName), data(data) {}
+
+
+	void PrintHuman()
+	{
+		cout << ID<< LastName<< Name<< MiddleName << &;
+	}
+
+};
+
+
+int main()
+{
+	Data A = Data(9, "May", 1984);
+	Human B = Human(1, "Doe", "John", "unknownovich", A);
+
+	    B.PrintHuman(); /*A.PrintData(); */
+
+	
+}
